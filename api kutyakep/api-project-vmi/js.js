c04d491f88b600d8bc59c568e0019a063d6b92b2
Hoplessdove <meggymag12@gmail.com>
2023-06-06 09:20:59 +0200

    adfasdfdsafdsa

--- api kutyakep/api-project-vmi/js.js
+++ api kutyakep/api-project-vmi/js.js
@@ -7,3 +7,12 @@ function gen() {
     }
         )
 }
+function gen() {
+    fetch("https://dog.ceo/api/breed/beagle/images/random")
+    .then(adat => adat.json())
+    .then(res => {
+        document.getElementById("kep").src=res.message;
+        
+    }
+        )
+}
